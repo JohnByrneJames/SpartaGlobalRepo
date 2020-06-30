a811903ab72157f81b69419ec74a93267f200bec
John Byrne <neardem1@gmail.com>
2020-06-30 11:05:36 +0100

    Finished SQL test with good score and started a Cheatsheet for Python

--- HelloWorld.py
+++
@@ -1,4 +0,0 @@
-print("Hello World")
-
-# This is a common file demonstrating how to create the "Hello World" file in the
-# Python programming language.
---
+++ Python-Files/Helloworld.py
@@ -0,0 +1,26 @@
+print("Hello World") # This is a comment
+
+# This is a common file demonstrating how to create the "Hello World" file in the programming language Python
+
+# Variables
+x = 5
+y = "Hello World"
+z = False
+
+# Multiple Variable Assignment
+a, b, c = "Orange", "Banana", "Cherry"
+
+# print(x)
+# print(y)
+# print(z)
+
+# Indentation instead of { }
+if 5 > 2:
+    print("\nFive is greater than two!")
+
+"""
+Multi Line comment 
+Line 1...
+Line 2...
+Line 3...
+"""
